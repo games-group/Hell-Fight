24ce81d5d5d2a87a82ad74501db856f2323eb9b1
Jerry Jackson <49294431+xkcdjerry@users.noreply.github.com>
2019-06-22 22:28:06 +0800

    Added _EventHandler

--- classes.py
+++ classes.py
@@ -5,10 +5,44 @@ import utils
 import locals
 
 
+class Mouse:
+    def __init__(self):
+        self.pos = (0, 0)
+        self.down = False
+
+
+class _EventHandler:
+    def __init__(self):
+        self.keys = {}
+        self.mouse = Mouse()
+
+    def rewind(self):
+        self.keys.clear()
+
+    def update(self):
+        for i in pygame.event.get():
+            if i.type == pygame.KEYDOWN:
+                self.keys[i.key] = True
+            elif i.type == pygame.KEYUP and i.type in self.keys:
+                del self.keys[i.key]
+            elif i.type == pygame.MOUSEBUTTONDOWN:
+                self.mouse.pos = i.pos
+                self.mouse.down = True
+            elif i.type == pygame.MOUSEBUTTONUP:
+                self.mouse.pos = i.pos
+                self.mouse.down = False
+            elif i.type == pygame.MOUSEMOTION:
+                self.mouse.pos = i.pos
+            else:
+                pygame.event.post(i)
+
+
 class Game(utils.classes.Root):
     def __init__(self):
         super().__init__()
         self.win = pygame.display.set_mode(locals.SIZE)
+        self.handler = _EventHandler()
+        self.players = [Player()]
 
     def rewind(self):
         self.__init__()
@@ -18,7 +52,10 @@ class Game(utils.classes.Root):
             self.update()
 
     def update(self):
-        pass
+        self.handler.update()
+        keys = self.handler.keys[:]
+        for i in self.players:
+            i.update(keys)
 
     @staticmethod
     def stop():
--- utils/classes.py
+++ utils/classes.py
@@ -11,7 +11,7 @@ def __repr__(self):
         return "{name}({stuff})".format_map({"name": name, "stuff": args or keyword_args})
 
 
-class ReprMeta(type):
+class MyMeta(type):
     def __call__(cls, *args, **key_args):
         inst = super().__call__(*args, **key_args)
         inst._args = [repr(i) for i in args]
@@ -19,11 +19,14 @@ class ReprMeta(type):
         return inst
 
     def __new__(mcs, class_name, bases, class_dict):
+        for k in class_dict:
+            if k.lower() != k:
+                raise NameError("Bad name: %s" % k)
         class_dict.setdefault("__repr__", __repr__)
         class_dict.setdefault("__signature__", inspect.signature(class_dict["__init__"]))
         return super().__new__(mcs, class_name, bases, class_dict)
 
 
-class Root(metaclass=ReprMeta):
+class Root(metaclass=MyMeta):
     def __init__(self):
         pass
